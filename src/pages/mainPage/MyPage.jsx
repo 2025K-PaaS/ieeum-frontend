84462199a20cb79483589433b062223ebdca0ca9
nakyeong <yunna3425@naver.com>
2025-10-30 00:47:50 +0900

    Feat: 마이페이지 API 연결

--- src/pages/mainPage/MyPage.jsx
+++ src/pages/mainPage/MyPage.jsx
@@ -1,16 +1,47 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import * as S from './MyPage.styled';
 import { useNavigate } from 'react-router-dom';
 import { Banner } from '../../components/Banner';
 import { Navbar } from './../../components/Navbar';
 import { Resource } from './../../components/Resource';
-import { resourceData } from './../../constant/resourceData';
 import Logo from '../../assets/icons/logo-white.svg';
+import axiosInstance from '../../apis/axiosInstance';
 
 const MyPage = () => {
     const navigate = useNavigate();
 
     const [activeTab, setActiveTab] = useState('공급자');
+    const [resourceData, setResourceData] = useState([]);
+
+    const handleMyRegistration = useCallback(async () => {
+        try {
+            const response = await axiosInstance.get('/resources/myresource');
+            console.log('등록한 자원', response.data);
+            setResourceData(response.data.resources);
+        } catch(error) {
+            console.log('등록한 자원 가져오기 실패', error.response);
+            setResourceData([]);
+        }
+    }, [])
+
+    const handleMyRequest = useCallback(async () => {
+        try {
+            const response = await axiosInstance.get('/requests/me');
+            console.log('요청한 자원', response.data);
+            setResourceData(response.data.requests);
+        } catch(error) {
+            console.log('요청한 자원 가져오기 실패', error.response);
+            setResourceData([]);
+        }
+    }, [])
+
+    useEffect(() => {
+        if (activeTab === '공급자') {
+            handleMyRegistration();
+        } else if (activeTab === '수요자') {
+            handleMyRequest();
+        }
+    }, [activeTab, handleMyRegistration, handleMyRequest]);
 
     return (
         <S.Wrapper>
@@ -40,17 +71,39 @@ const MyPage = () => {
                     </S.TextButton>
                 </S.TextButtonWrapper>
                 <S.ResourceWrapper>
-                    {resourceData.map((resource, index) => (
-                        <Resource
-                            key={index}
-                            name={resource.name}
-                            type={resource.type}
-                            material={resource.material}
-                            image={resource.image}
-                            state={resource.state}
-                            onClick={() => navigate('/matchingapplication')}
-                        />
-                    ))}
+                    {resourceData.map((resource, index) => {
+                        const path = activeTab==="공급자"
+                            ? `/registration/${resource.resource_id}` 
+                            : `/request/${resource.request_id}`;
+
+                        const headerTitle = activeTab==="공급자"
+                            ? "자원 등록" 
+                            : "자원 요청";
+
+                        return (
+                            <Resource
+                                key={index}
+                                name={resource.title}
+                                type={resource.item_name || resource.wanted_item}
+                                material={resource.material_type}
+                                image={resource.image_url}
+                                state={resource.status}
+                                onClick={() => navigate(path, {
+                                state: {
+                                    resource_id: resource.analysis_id || resource.request_id,
+                                    title: resource.title,
+                                    description: resource.description,
+                                    count: resource.amount || resource.desired_amount,
+                                    value: resource.value,
+                                    material: resource.material_type,
+                                    type: resource.item_name || resource.wanted_item,
+                                    isAutoMatch: false,
+                                    image: resource.image_url,
+                                    header_title: headerTitle,
+                                }
+                            })}
+                            />
+                    )})}
                 </S.ResourceWrapper>
             </S.ActivityWrapper>
             <Navbar />
